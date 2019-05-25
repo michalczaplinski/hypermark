cd923d4de8f19c98ca5edf995fab63b768a07d14
Michal Czaplinski <mmczaplinski@gmail.com>
2019-05-25 15:58:52 -0500

    update the release script

--- release.sh
+++ release.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
+set -o pipefail
 
+# This script takes care of all the release stuff
+# Usage: 
+# ./release.sh --patch | --minor | --major
 
-version=$(npm version minor)
-hub release create -d -m "Release $version" $version
+case "$1" in
+    --patch) 
+      version=patch
+      ;;
+    --minor)
+      version=minor
+      ;;
+    --major)
+      version=major
+      ;;
+    *)
+      echo "Error: Unsupported argument $1" >&2
+      echo "Usage: ./release.sh --patch | --minor | --major" >&2
+      exit 1
+      ;;
+esac
+
+version_name=$(npm version $version)
+hub release create -d -m "Release $version_name" $version_name
 git push
