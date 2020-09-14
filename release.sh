cd93cd06b2582176132c864d021c733841538d67
Michal Czaplinski <mmczaplinski@gmail.com>
2020-09-13 23:07:40 -0500

    update release script

--- release.sh
+++ release.sh
@@ -24,5 +24,12 @@ case "$1" in
 esac
 
 version_name=$(npm version $version)
+version_number=$(cat package.json | grep version | grep -o "\d\.\d\.\d")
+
+echo "Releasing a ${version} version number ${version_number}\n"
+
+echo "Creating a new release\n"
 hub release create -d -m "Release $version_name" $version_name
+
 git push
+git push --tags
